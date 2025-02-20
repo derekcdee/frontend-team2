b79ca765565f2dab44d386b8e5ba44f82d4a36cc
derekcdee <dcdailey@asu.edu>
2025-02-19 20:04:26 -0700

    initial backend connection

--- src/util/requests.js
+++ src/util/requests.js
@@ -1,1 +1,45 @@
 import $ from 'jquery';
+
+
+export function _ajax(settings={}) {
+    settings.url = `http://localhost:5000${settings.url}`;
+
+    return $.ajax(settings)
+        .then((res) => {
+            console.log('AJAX Reponse:', res);
+            return res; 
+        }).catch((err) => {
+            console.error('AJAX Request Failed:', err);
+            throw err;
+        });
+
+        // .then((res) => {
+        //     const response = JSON.parse(res);
+
+        //     if (typeof response.errors === 'string') {
+        //         // receiveResponse();
+        //         return Promise.reject();
+        //     } else {
+        //         return response;
+        //     }
+        //     return res;
+        // })
+}
+
+// test backend call
+export function test() {
+    _ajax({
+        url: "/products",
+        method: "GET",
+    });
+}
+
+/*==============================================================
+# Users
+==============================================================*/
+
+
+
+/*==============================================================
+# Products
+==============================================================*/

0aa36a8a5d771a798412fc30bb5a3c4e544de8d5
derekcdee <dcdailey@asu.edu>
2025-03-20 15:39:32 -0700

    logout

--- src/components/pages/user/UserLayout.jsx
+++ src/components/pages/user/UserLayout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { logout } from "../../../util/requests";
 import { updateUser } from "../../../util/redux/actionCreators";
 import { receiveResponse } from "../../../util/notifications";
 
 export default function UserLayout() {
+    const navigate = useNavigate();
     const user = useSelector(state => state.user);
     const isAdmin = user?.role === "Admin";
 
@@ -13,13 +14,14 @@ export default function UserLayout() {
         logout()
             .then((res) => {
                 receiveResponse(res);
-                dispatch(updateUser({
+                updateUser({
                     authenticated: false,
                     email: null,
                     firstName: null,
                     lastName: null,
                     role: null
-                }));
+                });
+                navigate('/');
             })
     };
 

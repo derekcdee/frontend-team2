69363d05598d66935580e771f8b732bf0a83cc84
Brian Boyer <beejboyer2@cox.net>
2025-03-31 13:31:51 -0700

    main

--- src/components/pages/BuildACuePage.jsx
+++ src/components/pages/BuildACuePage.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {Suspense} from "react";
+import { createRoot } from 'react-dom/client';
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 export default function BuildACuePage () {
     return (
         <div>
             build a cue
+            <Cue />
         </div>
     );
 }
+
+export function Cue() {
+    return (
+        <div>
+            <Canvas>
+                <Scene />
+                <ambientLight intensity={0.1} />
+                <directionalLight position={[0, 0, 5]} color="red" />
+                <OrbitControls />
+            </Canvas>
+        </div>
+    )
+  }
+
+  export function Scene(){
+    const gltf = useLoader(GLTFLoader, '../models/CueModel_v1.glb')
+    return <primitive object={gltf.scene} />
+  }
+
+createRoot(document.getElementById('root')).render(<Cue />)
